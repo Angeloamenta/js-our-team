79173018f4fea4500bad464de148a3c87aa70ea6
Angeloamenta <angeloamenta7@gmail.com>
2021-12-09 15:25:34 +0100

    bonus

--- js/script.js
+++ js/script.js
@@ -33,13 +33,23 @@ const team = [
 
 // Ogni membro ha le informazioni necessarie per stampare la relativa card: Nome, Ruolo e Foto.
 // Prendendo come riferimento la card di esempio presente nell’html, stampiamo dinamicamente una card per ogni membro del team.
+
+
 const container = document.querySelector(".team-container");
 const teamCard = document.querySelector(".team-card");
-console.log(teamCard);
+
+
+// faccio un ciclo for che giri su tutto l'array teams
 
 for (let i = 0; i < team.length; i++) {
+
+  // creo una costante che rappresenti un elemento per ogni ciclo 
+
   const element = team[i];
   console.log(element);
+
+  // inserisco nell'html con += la mia stringa con ogni elemento cambiato in base al giro che compio sull'array
+
 
    container.innerHTML += 
    `
@@ -56,5 +66,43 @@ for (let i = 0; i < team.length; i++) {
             </div>
           </div>
    `
-  
 }
+const inputName = document.getElementById('name');
+const inputRole = document.getElementById('role');
+const inputImg = document.getElementById('image');
+const button = document.getElementById("addMemberButton")
+
+button.addEventListener("click", function () {
+  let nameVal = inputName.value;
+  console.log(nameVal);
+  let roleVal = inputRole.value;
+  console.log(roleVal);
+  let imgVal = inputImg.value;
+  console.log(imgVal);
+
+  const object = 
+  {
+    name: nameVal,
+    role: roleVal,
+    image: imgVal,
+  };
+
+  team.push(object);
+  console.log(team);
+
+  container.innerHTML += 
+   `
+   <div class="team-card">
+            <div class="card-image">
+              <img
+                src="img/${imgVal}"
+                alt="Wayne Barnett"
+              />
+            </div>
+            <div class="card-text">
+              <h3>${nameVal}</h3>
+              <p>${roleVal}</p>
+            </div>
+          </div>
+   `
+})
